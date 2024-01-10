59cb107e34e59128591886cfeafc64a55b94ad18
Carlota <carlota.cdb221@gmail.com>
2024-01-10 18:23:55 +0100

    S ha implementado el activity_main.xml añadiendole un BottomAppBar además de realizar la conexion de las diferentes vistas para ortorgarle una mayor funcionalidad, a parte de esto tambien se han añadido una serie de iconos para poder utilizarlos en la propia AppBar y tambien se ha conseguido solucionar ciertos errores por el uso del modo noche.

--- app/src/main/java/com/example/elec_trade/Main.java
+++ app/src/main/java/com/example/elec_trade/Main.java
@@ -1,8 +1,10 @@
 package com.example.elec_trade;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+
 
 public class Main extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.bottomAppBar);
+        setSupportActionBar(toolbar);
     }
 }
--- app/src/main/java/com/example/elec_trade/Register.java
+++ app/src/main/java/com/example/elec_trade/Register.java
@@ -3,21 +3,37 @@ package com.example.elec_trade;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
 
+    private Button su;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        su = findViewById(R.id.register);
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             // Configura el color del título, por ejemplo
             actionBar.setTitle(Html.fromHtml("<font color=\"#F2A71B\">Register</font>"));
         }
+
+        su.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toMain = new Intent(Register.this, Main.class);
+                toMain.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                toMain.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(toMain);
+            }
+        });
 
     }
 }
